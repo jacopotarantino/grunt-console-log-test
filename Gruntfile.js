5f90fd6be604c37a32612adb167c94a6029d511a
Jack <jack@ofspades.com>
2014-09-22 20:16:07 -0400

    Don't push on version bump.

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function (grunt) {
         createTag: true,
         tagName: '%VERSION%',
         tagMessage: 'Version %VERSION%',
-        push: true,
+        push: false,
         pushTo: 'upstream',
         gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
       }
